7a2f76905a5627556e5fe91e696b40d64df17951
Musfira Ahmed <ahmedmusfira3@gmail.com>
2025-07-01 04:07:53 -0700

    Add files via upload

---
+++ week_01_tasks/Week_01_Task 02.py
@@ -0,0 +1,8 @@
+#Task 2:
+
+
+print ("""
+       My Name is Musfira Ahmed.
+       My hobby is reading novel books.
+       I am 18 years old.
+       I want to learn python to build a successfull career in Artificial Intelligence.""")
