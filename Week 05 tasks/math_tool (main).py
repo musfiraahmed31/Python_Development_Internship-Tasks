4eac36042d36eaedb19203ab66bd4fff0c4cac28
Musfira Ahmed <ahmedmusfira3@gmail.com>
2025-07-21 03:34:13 -0700

    Delete Week 05 tasks/math_tool (main).py

--- Week 05 tasks/math_tool (main).py
+++
@@ -1,7 +0,0 @@
-from math_tools import multiply
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-result = multiply(num1, num2)
-print("The result is:", result)
