056ef80a81837c7fb6bb10b6600c3413420ad24f
Musfira Ahmed <ahmedmusfira3@gmail.com>
2025-07-27 00:01:33 -0700

    Add files via upload

---
+++ Week 06 tasks/My Custom Package/main.py
@@ -0,0 +1,7 @@
+from mypackage import operations
+
+Result1 = operations.AddNumbers(5, 3)
+Result2 = operations.MultiplyNumbers(4, 6)
+
+print("Addition Result:", Result1)
+print("Multiplication Result:", Result2)
